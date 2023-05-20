870e20a8cf2c6eb69f90fd274b87217caee037c8
Mateo Toquica Arango <mtoquica@quantum-x.io>
2023-05-20 16:12:09 -0500

    ADDING pages

--- bilongoMusic/pages/views.py
+++ bilongoMusic/pages/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
+def about(request):
+    return render(request, 'pages/about.html')
