13d926d7f6b927e2b106d852932c491bcaa0d2f3
Mateo Toquica Arango <mtoquica@quantum-x.io>
2023-05-28 18:01:05 -0500

    Agregando perfiles

--- bilongoMusic/accounts/views.py
+++ bilongoMusic/accounts/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        # registro de usuarios
+        pass
+    else:
+        return render(request, 'accounts/register.html')
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        # login de usuarios
+        pass
+    else:
+        return render(request, 'accounts/login.html')
 def logout():
     return redirect('index')
 def dashboard(request):
--- bilongoMusic/bilongoMusic/settings.py
+++ bilongoMusic/bilongoMusic/settings.py
@@ -138,3 +138,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# MENSAJES
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
